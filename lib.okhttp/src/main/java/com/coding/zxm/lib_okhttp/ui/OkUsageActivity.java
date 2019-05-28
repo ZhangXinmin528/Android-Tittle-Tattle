9d968f62976a882e6145bc4c3b0dfc7d1372f236
ZhangXinmin528 <zhangxinmin528@sina.com>
2019-05-28 20:04:57 +0800

    analyse okhttp

---
+++ lib.okhttp/src/main/java/com/coding/zxm/lib_okhttp/ui/OkUsageActivity.java
@@ -0,0 +1,24 @@
+package com.coding.zxm.lib_okhttp.ui;
+
+import com.coding.zxm.libcore.ui.BaseActivity;
+
+/**
+ * Created by ZhangXinmin on 2019/5/28.
+ * Copyright (c) 2018 . All rights reserved.
+ */
+public class OkUsageActivity extends BaseActivity {
+    @Override
+    protected Object setLayout() {
+        return null;
+    }
+
+    @Override
+    protected void initParamsAndValues() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+}
